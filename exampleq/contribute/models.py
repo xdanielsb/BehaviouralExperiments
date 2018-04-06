f96c24cfcc9297629f7e1eadb91ec43a93723167
Daniel Santos <dfsantosbu@unal.edu.co>
2018-04-06 13:55:22 -0400

    Add global variables.

--- exampleq/contribute/models.py
+++ exampleq/contribute/models.py
@@ -7,14 +7,18 @@ from otree.api import (
 author = 'Your name here'
 
 doc = """
-Your app description
+This is a three player game where each player is initially endowed with 100 points. Each player individually makes a decision about how many of their points they want to contribute to the group. The combined contributions are multiplied by 2, and then divided evenly three ways and redistributed back to the players.
 """
 
 
 class Constants(BaseConstants):
     name_in_url = 'contribute'
-    players_per_group = None
+    players_per_group = 3
     num_rounds = 1
+
+    endowment = c(100)
+    multiplier = 2
+
 
 
 class Subsession(BaseSubsession):
@@ -22,8 +26,9 @@ class Subsession(BaseSubsession):
 
 
 class Group(BaseGroup):
-    pass
+    total_contribution = models.CurrencyField()
+    individual_share = models.CurrencyField()
 
 
 class Player(BasePlayer):
-    pass
+    contribution = models.CurrencyField(min=0, max=Constants.endowment)
